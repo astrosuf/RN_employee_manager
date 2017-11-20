8dcbc7774f89767ef149913f808aac648ed81867
Sufiyaan Mitha <sufiyaan.mitha@gmail.com>
2017-11-20 21:15:31 +1300

    Added EmployeeForm and EmployeeEdit functionality

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import {Scene, Router, Actions, Stack} from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
     return (
@@ -14,20 +15,23 @@ const RouterComponent = () => {
                 </Scene>
 
                 <Scene key="main">
+
                     <Scene 
                         key="employeeList" 
                         component={EmployeeList} 
                         title="Employees"
                         rightTitle = "Add"
-                        onRight = {() => Actions.employeeCreate()}
-                    />
+                        onRight = {() => Actions.employeeCreate()}/>
 
                     <Scene 
-                            key="employeeCreate" 
-                            component={EmployeeCreate} 
-                            title="Create Employee"
-                    />
-        
+                        key="employeeCreate" 
+                        component={EmployeeCreate} 
+                        title="Create Employee" />
+                    
+                    <Scene
+                        key="employeeEdit"
+                        component={EmployeeEdit}
+                        title = "Edit Employee" />      
                 </Scene>
             </Scene>
         </Router>
